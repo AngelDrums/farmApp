374db02de98c550019b1eb47475f61df49c51e0f
EduardGodi <angelmaster.ag@gmail.com>
2024-04-14 01:49:34 -0600

    Se agrego Bootstrap al proyecto

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
-alert("Bienvenido a FarmApp - Tu farmacia de confianza \n\nNuestra plataforma esta en mantenimiento\npor ahora solo puedes comprar 1 producto a la vez. ");
+alert("Bienvenido a FarmApp - Tu farmacia de confianza \n\nNuestra plataforma esta en mantenimiento 🛠️\npor ahora solo puedes comprar 1 producto a la vez. ");
 let producto;
 let cantidad;
 let total;
 let seguirComprando = true;
+let subTotal = function (a, b) {
+    switch (a) {
+        case 1:
+            // Aspirinas
+            return 1 * b;
+        case 2:
+            // Panadol Ultra
+            return 2 * b;
+        case 3:
+            // Bioelectro
+            return 4 * b;
+        case 4:
+            // Tabcin gripe y tos
+            return 3 * b;
+    }
+}
 do {
     producto = parseInt(prompt("Productos: \n 1.- Aspirina $1 \n 2.- Panadol Ultra $2 \n 3.- Bioelectro $4 \n 4.- Tabcin gripe y tos $3"));
     if (producto < 5 && producto > 0) {
@@ -10,22 +26,6 @@ do {
 
         if (!isNaN(producto) && !isNaN(cantidad)) {
             // si es número
-            let subTotal = function (a, b) {
-                switch (a) {
-                    case 1:
-                        // Aspirinas
-                        return 1 * b;
-                    case 2:
-                        // Panadol Ultra
-                        return 2 * b;
-                    case 3:
-                        // Bioelectro
-                        return 4 * b;
-                    case 4:
-                        // Tabcin gripe y tos
-                        return 3 * b;
-                }
-            }
             total = subTotal(producto, cantidad);
             alert(`El total a pagar es: ${total}`)
         } else {
