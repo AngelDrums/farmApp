207fd04f6290b1a840b32d6f7f5c67dd942c34ad
EduardGodi <angelmaster.ag@gmail.com>
2024-05-22 02:24:06 -0600

    Se empezo a trabajar con el Dom

--- js/main.js
+++ js/main.js
@@ -43,63 +43,138 @@
 //               SEGUNDA PRE-ENTREGA (Refactorización)
 // -------------------------------------------------------------
 
-const productos = [
-    {
-        nombre: "Panadol Ultra",
-        precio: 2,
-    },
-    {
-        nombre: "Aspirina",
-        precio: 1,
-    },
-    {
-        nombre: "BioElectro",
-        precio: 3,
-    },
-    {
-        nombre: "Tabcin Gripe y Tos",
-        precio: 2,
-    }
-]
+// const productos = [
+//     {
+//         nombre: "Panadol Ultra",
+//         precio: 2,
+//     },
+//     {
+//         nombre: "Aspirina",
+//         precio: 1,
+//     },
+//     {
+//         nombre: "BioElectro",
+//         precio: 3,
+//     },
+//     {
+//         nombre: "Tabcin Gripe y Tos",
+//         precio: 2,
+//     }
+// ]
 
-function mostrarProductos(){
-    let mensaje='';
-    let cntTitulo=0;
-    let noProducto = 0;
-    productos.forEach(el => {
-        noProducto++
-        if(cntTitulo===0){
-            mensaje += 'Productos: \n';
-            cntTitulo++;
-        }
-        mensaje += noProducto + ". " + el.nombre + " " +"$" + el.precio + "\n"
+// function mostrarProductos(){
+//     let mensaje='';
+//     let cntTitulo=0;
+//     let noProducto = 0;
+//     productos.forEach(el => {
+//         noProducto++
+//         if(cntTitulo===0){
+//             mensaje += 'Productos: \n';
+//             cntTitulo++;
+//         }
+//         mensaje += noProducto + ". " + el.nombre + " " +"$" + el.precio + "\n"
+//     });
+//     return mensaje;
+// }
+
+// let seleccionado = (el,cntd) =>{
+//     let subtotal = productos[el].precio*cntd;
+//     alert(`El total de su compra es: $${subtotal}`);
+// }
+
+// let seguirComprando = true;
+
+// do{
+
+//     let producto = parseInt(prompt(mostrarProductos()));
+    
+    
+//     if(producto>0 && producto<=productos.length){
+//         producto--;
+//         let cantidad = parseInt(prompt("¿Cuantos desea comprar?"));
+//         seleccionado(producto, cantidad);
+//     }else{
+//         alert("No existe el producto :(");
+//     }
+
+//     seguirComprando = confirm('¿Quieres Seguir comprando?');
+// }while(seguirComprando)
+
+// --------------------------------------------------------------
+//              TERCERA PREENTREGA - DOM 
+// --------------------------------------------------------------
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+const botones = document.getElementById("button__header");
+const container = document.getElementById("container");
+
+function crearCard(arrayMedicamentos){
+
+    const card = document.createElement("div");
+    card.className = "card"
+
+    const img = document.createElement("img");
+    img.src = arrayMedicamentos.img;
+    img.className = "imgProductos";
+
+    const tituloMedicamento = document.createElement("p");
+    tituloMedicamento.innerText = arrayMedicamentos.nombre;
+    tituloMedicamento.className = "tituloMedicamento";
+    
+
+    const precioMedicamento = document.createElement("p");
+    precioMedicamento.innerText = `$${arrayMedicamentos.precio}`;
+    precioMedicamento.className = "precioMedicamento";
+    
+    const agregarAlCarrito = document.createElement("button");
+    agregarAlCarrito.innerText = "Agregar";
+    agregarAlCarrito.onclick = () =>{
+        carrito.push(arrayMedicamentos);
+        console.log(carrito);
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+    };
+
+    card.append(img, tituloMedicamento, precioMedicamento, agregarAlCarrito);
+    container.append(card);
+}
+
+
+function mostrarMedicamentos(){
+    container.innerText="";
+    medicamentos.forEach(el => {
+        crearCard(el)
     });
-    return mensaje;
 }
 
-let seleccionado = (el,cntd) =>{
-    let subtotal = productos[el].precio*cntd;
-    alert(`El total de su compra es: $${subtotal}`);
+function mostrarCarrito(arrayCarrito){
+    container.innerText="";
+    arrayCarrito.forEach(el => {
+        crearCard(el);
+    });
 }
 
-let seguirComprando = true;
+if(container.innerText != "" ){
+    console.log("El container no esta vacío");
+}else{
+    
+    medicamentos.forEach(el => {
+        crearCard(el)
+    });
+}
 
-do{
 
-    let producto = parseInt(prompt(mostrarProductos()));
-    
-    
-    if(producto>0 && producto<=productos.length){
-        producto--;
-        let cantidad = parseInt(prompt("¿Cuantos desea comprar?"));
-        seleccionado(producto, cantidad);
-    }else{
-        alert("No existe el producto :(");
-    }
+const btnProductos = document.createElement("button");
+btnProductos.innerText = "Productos";
+btnProductos.className = "btnProductos font--button--poppins--600"
 
-    seguirComprando = confirm('¿Quieres Seguir comprando?');
-}while(seguirComprando)
-    
-    
-    
-    
+const btnCarrito = document.createElement("button");
+btnCarrito.innerText = "Carrito";
+btnCarrito.className = "btnProductos font--button--poppins--600"
+
+botones.appendChild(btnProductos);
+botones.appendChild(btnCarrito);
+
+btnProductos.onclick= () => mostrarMedicamentos();
+btnCarrito.onclick = () => mostrarCarrito(carrito);
+
+
---
+++ js/medicamentos.js
@@ -0,0 +1,38 @@
+const medicamentos = [
+    {
+        nombre: "Acetamenofen",
+        precio: 1,
+        img: "https://res.cloudinary.com/dnicoqub3/image/upload/v1716346383/appFarm/acetamenofen.jpg"
+    },
+    {
+        nombre: "Aleve max",
+        precio: 2,
+        img: "https://res.cloudinary.com/dnicoqub3/image/upload/v1716346383/appFarm/aleve-max.jpg"
+    },
+    {
+        nombre: "Dorival",
+        precio: 2,
+        img: "https://res.cloudinary.com/dnicoqub3/image/upload/v1716346383/appFarm/dorival.jpg"
+    },
+    {
+        nombre: "Aspirina Forte",
+        precio: 1,
+        img: "https://res.cloudinary.com/dnicoqub3/image/upload/v1716346383/appFarm/aspirina-forte.jpg"
+    },
+    {
+        nombre: "Tabcin día",
+        precio: 2,
+        img: "https://res.cloudinary.com/dnicoqub3/image/upload/v1716346383/appFarm/tabcin-dia.jpg"
+    },
+    {
+        nombre: "Tabcin gripe y tos",
+        precio: 3,
+        img: "https://res.cloudinary.com/dnicoqub3/image/upload/v1716346384/appFarm/tabcin-gripeytos.jpg"
+    },
+    {
+        nombre: "Bio Zink",
+        precio: 10,
+        img: "https://res.cloudinary.com/dnicoqub3/image/upload/v1716346383/appFarm/bio-zink.jpg"
+    },  
+   
+];
